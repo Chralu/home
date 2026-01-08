9c1880ce45d24d38a16f0efa45ec8c1851880335
Chralu <contact@charly-caulet.net>
2026-01-08 15:03:40 +0100

    feat(fish): remove helix shortcuts.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,12 +2,6 @@ function fish_user_key_bindings
     # Execute this once per mode that emacs bindings should be used in
     fish_default_key_bindings -M insert
 
-    # Then execute the vi-bindings so they take precedence when there's a conflict.
-    # Without --no-erase fish_vi_key_bindings will default to
-    # resetting all bindings.
-    # The argument specifies the initial mode (insert, "default" or visual).
-    fish_helix_key_bindings --no-erase insert
-
     # Nullify fzf default keybindings
     bind \cT '' -M insert
     bind \cR '' -M insert
